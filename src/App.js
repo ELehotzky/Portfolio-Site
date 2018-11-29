121804a7dbdb666d937b1cc012aa9bac9d3bfcc9
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-29 10:13:27 -0600

    created contact and work components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav.js";
+import Work from "./components/Work.js";
 import Footer from "./components/Footer.js";
 
 class App extends Component {
   render() {
     return (
-      <div>
-      	        {/*<!-- Wrapper-->*/}
       <div id="wrapper">
 
       	<Nav />
@@ -28,88 +27,6 @@ class App extends Component {
                   <img src="images/me.jpg" alt="" />
                 </a>
               </article>
-
-            {/*<!-- Work -->*/}
-              <article id="work" className="panel">
-                <header>
-                  <h2>Work</h2>
-                </header>
-                <p>
-                  Phasellus enim sapien, blandit ullamcorper elementum eu, condimentum eu elit.
-                  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-                  luctus elit eget interdum.
-                </p>
-                <section>
-                  <div className="row">
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic01.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic02.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic03.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic04.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic05.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic06.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic07.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic08.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic09.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic10.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic11.jpg" alt="" /></a>
-                    </div>
-                    <div className="col-4 col-6-medium col-12-small">
-                      <a href="#" className="image fit"><img src="images/pic12.jpg" alt="" /></a>
-                    </div>
-                  </div>
-                </section>
-              </article>
-
-            {/*<!-- Contact -->*/}
-              <article id="contact" className="panel">
-                <header>
-                  <h2>Contact Me</h2>
-                </header>
-                <form action="#" method="post">
-                  <div>
-                    <div className="row">
-                      <div className="col-6 col-12-medium">
-                        <input type="text" name="name" placeholder="Name" />
-                      </div>
-                      <div className="col-6 col-12-medium">
-                        <input type="text" name="email" placeholder="Email" />
-                      </div>
-                      <div className="col-12">
-                        <input type="text" name="subject" placeholder="Subject" />
-                      </div>
-                      <div className="col-12">
-                        <textarea name="message" placeholder="Message" rows="6"></textarea>
-                      </div>
-                      <div className="col-12">
-                        <input type="submit" value="Send Message" />
-                      </div>
-                    </div>
-                  </div>
-                </form>
-              </article>
-
-          </div>
 
           <Footer />
 
