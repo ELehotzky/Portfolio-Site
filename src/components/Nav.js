18f5e715570e32161cadc64601227d56ee5a7d1b
ELehotzky <Lehotzky.Erica@gmail.com>
2018-12-10 18:30:22 -0600

    added additional links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,13 @@ export default class Nav extends Component {
 	render() {
 		return (
           <nav id="nav">
-            <a href="/" className="icon fa-home"><span>Home</span></a>
-            <a href="/work" className="icon fa-folder"><span>Work</span></a>
-            <a href="/contact" className="icon fa-envelope"><span>Contact</span></a>
-            <a href="https://twitter.com/ericalehotzky" target="_blank" className="icon fa-twitter"><span>Twitter</span></a>
+            {/*Icons taken from fontawesome.com*/}
+            <a href="/" className="icon fa-home" aria-hidden="true"><span>Home</span></a>
+            <a href="/work" className="icon fa-folder" aria-hidden="true"><span>Work</span></a>
+            <a href="/contact" className="icon fa-envelope" aria-hidden="true"><span>Contact</span></a>
+            <a href="https://twitter.com/ericalehotzky" target="_blank" className="icon fa-twitter" aria-hidden="true"><span>Twitter</span></a>
+            <a href="https://www.linkedin.com/in/ericalehotzky/" target="_blank" className="icon fa-linkedin-square" aria-hidden="true"><span>LinkedIn</span></a>
+            <a href="https://github.com/elehotzky" target="_blank" className="icon fa-github" aria-hidden="true"><span>GitHub</span></a>
           </nav>
 		);
 	}
